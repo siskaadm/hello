c95d9507164bdd98fb81936e3edb09e709f4f8bf
siskaadm <siskadm1617@gmail.com>
2022-05-13 14:57:18 +0700

    menu

--- resources/views/sections/sidebar.blade.php
+++ resources/views/sections/sidebar.blade.php
@@ -39,6 +39,29 @@
                 data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
        with font-awesome or any other icon font library -->
+       <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            Menu
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ asset('') }}assets/login.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Login</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ asset('') }}assets/logout.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Logout</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
